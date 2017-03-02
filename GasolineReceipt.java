e32c81c58a99176450e92bd4fef13ea42e71feb1
Paul Pistorius <jpistoriusp@gmail.com>
2017-03-02 13:19:16 -0700

    add excerise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|        7-Eleven        |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2017-03-02  01:14PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       10.870  |" );
+		System.out.println( "| Price/gallon: $ 1.999  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:  $ 21.72   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
