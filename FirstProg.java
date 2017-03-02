0708180d872ebd8747144f0263697a6800bf1fe3
Paul Pistorius <Paul@Pauls-MacBook-Pro.local>
2017-03-02 11:49:31 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args) {
+		System.out.println( "I, Paul Pistorius, am determined to learn how to code. " );
+		System.out.println( "Today's date is March 2, 2017" );
+	}
+}
