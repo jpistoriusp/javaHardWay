059a248aa9e78b3a30c39cc38db826e81f0cd1d9
Paul Pistorius <jpistoriusp@gmail.com>
2017-03-02 17:51:59 -0700

    add exercise 4

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,6 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		System.out.println( "Alpha\nBrovo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis is a test.\n\nI am learning Java the Hard Way!" );
+		// All of exercise 3 simplified to one single line.
+	}
+}
